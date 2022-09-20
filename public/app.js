80d23119aa3e4e2ab472e2815ef85e72709accb5
hamwa03 <hampus.hjw.wallin@gmail.com>
2022-09-20 12:00:38 +0200

    late first commit but it's alive and has a foundation

---
+++ public/app.js
@@ -0,0 +1,9 @@
+console.log('I am alive >:D');
+
+add = () => {
+    firstName = document.getElementById('txtFirstName').value;
+    lastName = document.getElementById('txtLastName').value;
+    phoneNumber = document.getElementById('txtPhoneNumber').value;
+    adress = document.getElementById('txtAdress').value;
+
+}
